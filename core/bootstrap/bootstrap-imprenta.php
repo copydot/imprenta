a1ad91ea160e3abd30391ba7640f84eafb6d4e55
copydot <copydot@gmail.com>
2016-03-10 12:20:20 -0300

    Update bootstrap-imprenta.php

--- core/bootstrap/bootstrap-imprenta.php
+++ core/bootstrap/bootstrap-imprenta.php
@@ -1,90 +1,39 @@
 <?php
-class Request {
- 
-    public function __construct() {
-        $this->modulo = "";
-        $this->metodo = "";
-        $this->argumentos = array ();
- 
-        $path = filter_var($_SERVER["REQUEST_URI"], FILTER_SANITIZE_URL); # filtra caracteres de la URI
-        $url = str_replace(WEB_DIR, "", $path ); # quita el directorio web de la URI
-        $url = strtolower($url); # convierte toda la cadena a minusculas
-        $url = explode('/', $url); # crea un array dividido segun "/"
-        $url = array_filter($url); #elimina elementos no validos / falses
-        $cantidad = count($url); #cantidad de elementos en la URI
-        //echo $cantidad; exit ();
-        switch ($cantidad) {
-            case 0:  # si la uri es vacia se toman los valores por defecto definidos mas abajo  (creados en settings)
-                break;
- 
-            case 1: #Ej:  item/ o  item.html
-                $modulo = limpiar_extension ($url[0]);
-                $this->modulo = $modulo;
-                $this->metodo = DEFAULT_METODO;
-                break;
- 
-            case 2: #Ej:  item/listar/  o  item/listar.html
-                $this->modulo = array_shift($url); # almacena y quita el primer valor del array
-                $this->metodo = array_shift($url);
-                $this->argumentos = array(); // argumentos array
-                //$this->argumentos = $this->limpiar_extension ($url[0]); // toma el
-                break;
- 
-            case 3: #Ej:  agenda/categoria0/categoria1/ o  agenda/categoria0/categoria1.html
-            case 4: #Ej:  agenda/categoria0/categoria1/categoria2 o agenda/categoria0/categoria1/categoria2.html
-            case 5: #Ej:  agenda/categoria0/categoria1/categoria2 o agenda/categoria0/categoria1/categoria2.html
-            case 6: #Ej:  agenda/categoria0/categoria1/categoria2/1234/detalle_del_evento.html
-                $this->modulo = array_shift($url); # almacena y quita el primer valor del array
-                $indice = count($url);
-                array_push($url,$this->limpiar_extension(array_pop($url)));  // quita el ultimo elemento - array_pop- , le aplica la funcion y lo vuelve a agregar el ultimo elemento - array_push-
- 
-                if ((int)($url[$indice-2]) != 0){ // a la cantidad total se le quito uno y teniendo en cuenta que los indices parten de cero, chequeamos si el penultimo  es un numero
-                    $this->metodo = "ver";
-                } else {
-                    $this->metodo = "listar";
-                }
-                $this->argumentos = $url;
-                break;
- 
-            default:
-                $this->modulo = "error";
-                $this->metodo = "ver";
-                break;
-        } #end switch
- 
-        # CARGA POR DEFECTO
-        if (!$this->modulo) {
-            $this->modulo = DEFAULT_CONTROLLER; # definido como constante en index
-        }
- 
-        if (!$this->metodo) {
-            $this->metodo = DEFAULT_METODO; #  por defecto, definido como metodo abstracto en la clase Controler
-        }
- 
-        if(!isset($this->argumentos)) {
-            $this->argumentos =  array ();
-        }
-    }
- 
-    private function limpiar_extension($url){
-        $extension_html = strrchr($url, "."); # chequea si la url tiene extension ej: .html
-        if ($extension_html) {
-            return strrev(preg_replace(strrev("/$extension_html/"),"",strrev($url),1));
-        } else {
-            return $url;
-        }
-    }
- 
-    public function getModulo(){
-        return $this->modulo;
-    }
- 
-    public function getMetodo() {
-        return $this->metodo;
-    }
- 
-    public function getArgs(){
-        return $this->argumentos;
+class Bootstrap {
+	# encargada de llamar / ubicar / cargar los controladores - modelos y vistas - y creae el objeto controler correspondiente
+	# recibe un objeto request llamado desde el index con las propiedades api (si no), modulo, metodo, argumentos
+    public static function run(Request $request) { 
+			
+		$modulo = $request->getModulo(); # Nombre del MODULO
+		$metodo = $request->getMetodo(); # Nombre del METODO
+		$argumentos = $request->getArgs();
+		/*
+		echo ("Modulo: </br>");
+		var_dump($modulo); echo ("</br>");
+		echo ("Metodo: </br>");
+		var_dump($metodo); echo ("</br>");
+		echo ("Argumentos: </br>");
+		var_dump ($argumentos); echo ("</br>");
+		*/
+		//exit();
+		
+		$rutaController = ROOT.'modulos'.DS.$modulo.DS.$modulo.'-controller.php'; # ruta donde se encontraria el controlador
+		
+		if( is_readable($rutaController) ){ # si existe el archivo controller y es accesible
+			
+			$rutaView = ROOT.'modulos'.DS.$modulo.DS.$modulo.'-view.php'; # ruta donde se encontraria el controlador
+			$rutaModel = ROOT.'modulos'.DS.$modulo.DS.$modulo.'-model.php'; # ruta donde se encontraria el controlador
+			require_once $rutaModel;
+			require_once $rutaView;
+			require_once $rutaController;
+			//ucwords tranforma a mayuscula la primer letra. Determina el nombre de la clase del objeto a crear
+			$nombre_controlador = ucwords($modulo)."Controller"; 
+			// crea un objeto del tipo elegido*/
+			$controlador = new $nombre_controlador($metodo, $argumentos);
+			
+		} else { // si existe el archivo controller
+			throw new Exception('Controlador no encontrado'); // lanza una excepcion
+		} // fin if else si existe el archivo y es accesible
     }
 }
 ?>
